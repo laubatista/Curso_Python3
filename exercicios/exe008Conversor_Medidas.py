fecda3d31fadacec080e4c8c0ddd2bbc2fb6ac91
Laudeine Batista <laudeine_batista@hotmail.com>
2018-09-12 23:05:48 -0300

    exe008Conversor_Medidas.py

--- exercicios/exe008Conversor_Medidas.py
+++ exercicios/exe008Conversor_Medidas.py
@@ -7,3 +7,8 @@ hm = medida/ 100
 km = medida / 1000
 
 print('A distância de {}m equivale a:\ndecímetro = {:.0f}\ncentímetros = {:.0f}\nmilímetros = {:.0f}\ndecâmetro = {:.0f}\nhectômetro = {:.0f}\nkilômetro = {:.0f}'. format(medida,dm, cm, mm, dam, hm, km))
+
+
+
+
+
